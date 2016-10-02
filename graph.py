81f5c4210e047ffd1697ef15e0891b921fc374f4
Kurt van Bendegem <kurtv7348@gmail.com>
2016-10-02 12:29:58 -0400

    basic structure implemented

---
+++ graph.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+
+class Graph:
+    """
+    A class to encompass an undirected graph data structure
+
+    """
+
+    def __init__(self):
+        self.adjacency_list = {} # to hold the edges
+        self.num_edges = 0
+        self.num_vertices = 0
+        self.verticies = []
+
+    # The following methods return data about specific verticies
+
+    def adjacent(self, x, y):
+        if y in self.adjacency_list[x]:
+            return True
+
+        return False
+
+    def get_neighbours(self, id):
+        if id in self.adjacency_list:
+            return self.adjacency_list[id]
+        else:
+            return -1
+
+    # The following methods modify the structure of the graph
+
+    def add_vertex(self, id, value):
+        if id not in self.adjacency_list:
+            new_vertex = Vertex(id, value)
+            self.verticies.append(new_vertex)
+            self.adjacency_list[id] = []
+            self.num_vertices += 1
+            return 0
+        else:
+            return 1
+
+    def remove_vertex(self, id):
+        if self.adjacency_list.pop(id, None) != None:
+            for node in self.adjacency_list:
+                if id in self.adjacency_list[node]: #vertex exists so delete it
+                    self.adjacency_list[node].remove(id)
+                    i = 0
+                    while i < len(self.verticies) and self.verticies[i].id != id:
+                        i += 1
+                    self.verticies.pop(i)
+                    break
+            return 0
+        else:
+            return 1
+
+    def add_edge(self, x, y):
+        if y not in self.adjacency_list[x]:
+            self.adjacency_list[x].append(y)
+        if x not in self.adjacency_list[y]:
+            self.adjacency_list[y].append(x)
+        return 0
+
+    def remove_edge(self, x, y):
+        if y in self.adjacency_list[x]:
+            self.adjacency_list[x].remove(y)
+        else:
+            return 1
+        if x in self.adjacency_list[y]:
+            self.adjacency_list[y].remove(x)
+        else:
+            return 1
+
+        return 0
+
+    # Setter and getter for verticies
+    def get_vertex_value(self, id):
+
+        if id in self.adjacency_list:
+            for vertex in self.adjacency_list:
+                if vertex.id == id:
+                    return vertex.value
+
+
+    def set_vertex_value(self, id, new_value):
+        pass
+
+    # Method prototypes (Not implemented yet)
+    def get_edge_value(self, x, y):
+        pass
+    def set_edge_value(self, x, y, new_value):
+        pass
+
+    # string conversion: just return the adjacency list
+    def __str__(self):
+        return str(self.adjacency_list)
+
+
+class Vertex:
+    """
+    A class to represent a single node in a Graph
+    """
+
+    def __init__(self, id, value):
+        self.id = id
+        self.value = value
